6d37bf06a43a518e8fdbd5f679bb63417065eba1
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-08-31 01:45:54 +0000

    complete

--- main.cpp
+++ main.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 int main() {
-  fstream dataFile;
+  fstream dataFile; //Create a file
 
   cout << "Opening file...\n";
   dataFile.open("demofile.txt", ios::out); // Open newly created file for output.
+  dataFile << "Jones\n";  //Write line 1
+  dataFile << "Smith\n";  //Write line 2
+  dataFile << "Willis\n"; //Write line 3
+  dataFile <<  "Davis\n"; //Write line 4
+  cout << "Done.\n";
+  return 0;
+
 }
